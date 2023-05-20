1b552103a9168e538520addcef6c2e0acb0457fb
JEFFERSON ESTRADA JARAMILLO <jeffersonestrada249382@correo.itm.edu.co>
2023-05-19 23:20:16 -0500

    Model calculator

---
+++ CalculatorDef/CalculatorDef/MVVM/Models/CalculatorModel.cs
@@ -0,0 +1,36 @@
+﻿namespace CalculatorDef.Models
+{
+    public class CalculatorModel
+    {
+
+
+        public double PerformOperation(double operand1, double operand2, string operation)
+        {
+            double result = 0;
+
+            switch (operation)
+            {
+                case "+":
+                    result = operand1 + operand2;
+                    break;
+                case "-":
+                    result = operand1 - operand2;
+                    break;
+                case "*":
+                    result = operand1 * operand2;
+                    break;
+                case "/":
+                    if (operand2 != 0)
+                        result = operand1 / operand2;
+                    else
+                        throw new DivideByZeroException("Error: No es posible dividir por 0");
+                    break;
+                default:
+                    throw new ArgumentException("Operacion invalida");
+            }
+
+            return result;
+        }
+
+    }
+}
