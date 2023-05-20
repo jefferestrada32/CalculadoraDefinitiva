3d21142a6b94eb516ac75e3ed6692a4d0d8e6487
JEFFERSON ESTRADA JARAMILLO <jeffersonestrada249382@correo.itm.edu.co>
2023-05-19 22:32:33 -0500

    MVVM folder

---
+++ CalculatorDef/CalculatorDef/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace CalculatorDef;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ CalculatorDef/CalculatorDef/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace CalculatorDef;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
